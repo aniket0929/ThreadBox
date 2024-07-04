06a1c5892390f88b75bd0f85c141490ccd9eecc1
Aniket G <aniketgavate12345@gmail.com>
2024-07-04 20:29:46 +0530

    removed a import from rtefield.jsx

--- app/admin/posts/form/components/RTEField.jsx
+++ app/admin/posts/form/components/RTEField.jsx
@@ -3,7 +3,6 @@ import { useState } from 'react';
 import dynamic from 'next/dynamic';
 import 'react-quill/dist/quill.snow.css';
 import { usePostForm } from '../contexts/PostFormContext';
-import ReactQuill from 'react-quill';
 
 const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 
