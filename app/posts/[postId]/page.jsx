6469ae24b09f4678706a864a51278892e3f7e87d
Aniket G <aniketgavate12345@gmail.com>
2024-07-04 21:25:55 +0530

    added the contact page

--- app/posts/[postId]/page.jsx
+++ app/posts/[postId]/page.jsx
@@ -29,7 +29,6 @@ export default async function Page({params}){
       <AuthorCard authorId={post?.authorId}/>
       <h5 className="text-sm text-gray-500">{post?.timestamp?.toDate()?.toLocaleDateString()}</h5>
       </div>
-      {/* added backticks coz of minified react error */}
       <div dangerouslySetInnerHTML={{__html: post?.content}}></div>
        </section>
     </main>
